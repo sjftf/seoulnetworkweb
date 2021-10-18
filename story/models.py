ce83de91c708666b73c4f308a094083cef7ff470
sjftf <sj@ftf.studio>
2021-10-18 19:20:52 +0900

    1018

--- story/models.py
+++ story/models.py
@@ -28,4 +28,4 @@ class Story(models.Model):
     def __str__(self):
         return self.title
     def get_absolute_url(self):
-        return reverse('story:story_detail',args=[self.id,])
+        return reverse('story:story_detail',args=[self.id,]) 
